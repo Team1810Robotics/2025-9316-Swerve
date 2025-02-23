2c0a649aeb34d01766ec5382ca03f7438352477e
Curtis Gathright <cgathright@gmail.com>
2025-02-23 12:41:26 -0600

    updated command posting

--- 2025-9316-Swerve-Base/src/main/java/frc/robot/Robot.java
+++ 2025-9316-Swerve-Base/src/main/java/frc/robot/Robot.java
@@ -55,7 +55,9 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    CommandScheduler.getInstance().schedule(Commands.print(Double.toString(m_robotContainer.algaeSubsystem.getDistanceSensor())));
+  }
 
   @Override
   public void teleopExit() {}
