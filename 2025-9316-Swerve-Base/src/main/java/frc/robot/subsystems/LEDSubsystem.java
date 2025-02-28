193a4888aaa3ad551d27c1192f184d1d7deb6f68
cgathright133 <60550818+cgathright133@users.noreply.github.com>
2025-02-27 21:36:14 -0600

    Updates for LEDSubsystem

---
+++ 2025-9316-Swerve-Base/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,104 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.led.*;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class LEDSubsystem extends SubsystemBase {
+    private final CANdle m_candle = new CANdle(Constants.LEDConstants.CANDLE_ID);
+    private final int LEDCount = Constants.LEDConstants.NUM_OF_LEDS;
+    private Animation m_toAnimate = null;
+
+    public enum AnimationTypes {
+        ColorFlow,
+        Fire,
+        Larson,
+        Rainbow,
+        RgbFade,
+        SingleFade,
+        Strobe,
+        Twinkle,
+        TwinkleOff,
+        SetAll
+    }
+    private AnimationTypes m_currentAnimation;
+
+    public LEDSubsystem() {
+        configureCANdle();
+        changeAnimation(AnimationTypes.Strobe);
+    }
+    public void configureCANdle(){
+        CANdleConfiguration configAll = new CANdleConfiguration();
+        configAll.statusLedOffWhenActive = true;
+        configAll.disableWhenLOS = false;
+        configAll.stripType = LEDStripType.GRB;
+        configAll.brightnessScalar = 0.1;
+        configAll.vBatOutputMode = CANdle.VBatOutputMode.Modulated;
+        m_candle.configAllSettings(configAll, 100);
+    }
+
+    public void incrementAnimation(){ 
+        switch(m_currentAnimation) {
+            case ColorFlow: changeAnimation(AnimationTypes.Fire); break;
+            case Fire: changeAnimation(AnimationTypes.Larson); break;
+            case Larson: changeAnimation(AnimationTypes.Rainbow); break;
+            case Rainbow: changeAnimation(AnimationTypes.RgbFade); break;
+            case RgbFade: changeAnimation(AnimationTypes.SingleFade); break;
+            case SingleFade: changeAnimation(AnimationTypes.Strobe); break;
+            case Strobe: changeAnimation(AnimationTypes.Twinkle); break;
+            case Twinkle: changeAnimation(AnimationTypes.TwinkleOff); break;
+            case TwinkleOff: changeAnimation(AnimationTypes.ColorFlow); break;
+            case SetAll: changeAnimation(AnimationTypes.ColorFlow); break;
+        }
+    }
+
+    public void changeAnimation(AnimationTypes toChange) {
+        m_currentAnimation = toChange;
+        
+        switch (toChange) {
+            case ColorFlow:
+                m_toAnimate = new ColorFlowAnimation(128, 20, 70, 0, 0.7, LEDCount, ColorFlowAnimation.Direction.Forward);
+                break;
+            case Fire:
+                m_toAnimate = new FireAnimation(0.5, 0.7, LEDCount, 0.7, 0.5);
+                break;
+            case Larson:
+                m_toAnimate = new LarsonAnimation(0, 255, 46, 0, 1, LEDCount, LarsonAnimation.BounceMode.Front, 3);
+                break;
+            case Rainbow:
+                m_toAnimate = new RainbowAnimation(1, 0.1, LEDCount);
+                break;
+            case RgbFade:
+                m_toAnimate = new RgbFadeAnimation(0.7, 0.4, LEDCount);
+                break;
+            case SingleFade:
+                m_toAnimate = new SingleFadeAnimation(50, 2, 200, 0, 0.5, LEDCount);
+                break;
+            case Strobe:
+                m_toAnimate = new StrobeAnimation(240, 10, 180, 0, 98.0 / 256.0, LEDCount);
+                break;
+            case Twinkle:
+                m_toAnimate = new TwinkleAnimation(30, 70, 60, 0, 0.4, LEDCount, TwinkleAnimation.TwinklePercent.Percent6);
+                break;
+            case TwinkleOff:
+                m_toAnimate = new TwinkleOffAnimation(70, 90, 175, 0, 0.8, LEDCount, TwinkleOffAnimation.TwinkleOffPercent.Percent100);
+                break;
+            case SetAll:
+                m_toAnimate = null;
+                break;
+        }
+        System.out.println("Changed to " + m_currentAnimation.toString());
+    }
+    @Override
+    public void periodic(){
+        if (m_toAnimate == null){
+            m_candle.setLEDs(0,0,255); //Blue (Default)
+        }else{
+            m_candle.animate(m_toAnimate);
+        }
+    }
+    
+}
